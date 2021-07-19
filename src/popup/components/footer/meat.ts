6caca16081afe37a2bedd543236378967ad047e3
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-07-19 15:38:34 +0800

    Add meat link

---
+++ src/popup/components/footer/meat.ts
@@ -0,0 +1,6 @@
+import { Locale, locale } from "../../../util/i18n";
+const nowHour = new Date().getHours()
+
+if ((nowHour === 17 || nowHour === 18) && locale === Locale.ZH_CN) {
+    document.getElementById('meat-container').style.display = 'block'
+}
