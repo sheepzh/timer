1aa96df24ec946322f015f61c6fc4f2d373775a1
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-07-20 11:40:52 +0800

    Add 12a.m. for meat time

--- src/popup/components/footer/meat.ts
+++ src/popup/components/footer/meat.ts
@@ -1,6 +1,6 @@
 import { Locale, locale } from "../../../util/i18n";
 const nowHour = new Date().getHours()
 
-if ((nowHour === 17 || nowHour === 18) && locale === Locale.ZH_CN) {
+if ((nowHour === 17 || nowHour === 18 || nowHour === 12) && locale === Locale.ZH_CN) {
     document.getElementById('meat-container').style.display = 'block'
 }
