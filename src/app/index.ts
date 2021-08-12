5f0d67379642cf0ed4fc0f0c241c29460297c113
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-08-12 11:08:12 +0800

    Fix broken change of element-plus's i18n

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,31 @@
-import { App, createApp } from 'vue'
+import { App, createApp, defineComponent, h } from 'vue'
 import Main from './layout'
 import 'element-plus/lib/theme-chalk/index.css'
 import './styles' // global css
 import installRouter from './router'
 import '../common/timer'
-import '../common/element-plus'
+import { useLocale, useLocaleProps } from 'element-plus'
+import { locale as appLocale, Locale } from '../util/i18n'
 
-const app: App = createApp(Main)
-installRouter(app)
-app.mount('#app')
+const locales: { [locale in Locale]: () => Promise<{ default: unknown }> } = {
+    zh_CN: () => import('element-plus/lib/locale/lang/zh-cn'),
+    en: () => import('element-plus/lib/locale/lang/en'),
+    ja: () => import('element-plus/lib/locale/lang/ja')
+}
+
+// Use proxy for the i18n of element-plus 
+// @see https://element-plus.gitee.io/#/zh-CN/component/i18n
+const AppMainProxy = defineComponent({
+    props: { ...useLocaleProps },
+    setup() {
+        useLocale()
+        return () => h(Main)
+    }
+})
+
+const localeGetter = locales[appLocale]
+localeGetter().then(locale => {
+    const app: App = createApp(AppMainProxy, { locale: locale.default })
+    installRouter(app)
+    app.mount('#app')
+})
