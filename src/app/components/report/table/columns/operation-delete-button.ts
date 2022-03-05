8b9dbb1374b117b20735f620d5c19cc8e49f9676
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-03-05 16:47:55 +0800

    Optimise delete button

---
+++ src/app/components/report/table/columns/operation-delete-button.ts
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2022 Hengyang Zhang
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import { computed, defineComponent, h, PropType, Ref } from "vue"
+import OperationPopupConfirmButton from "@app/components/common/popup-confirm-button"
+import { Delete } from "@element-plus/icons"
+import { t } from "@app/locale"
+import { dateFormatter } from "../../formatter"
+import { formatTime } from "@util/time"
+
+const DISPLAY_DATE_FORMAT = '{y}/{m}/{d}'
+const deleteButtonText = t(msg => msg.item.operation.delete)
+
+/**
+ * Compute the confirm text for one item
+ * 
+ * @param url  item url
+ * @param date item date
+ */
+function computeSingleConfirmText(url: string, date: string): string {
+    const formatDate = dateFormatter(date)
+    return t(msg => msg.item.operation.deleteConfirmMsg, { url, date: formatDate })
+}
+
+function computeRangeConfirmText(url: string, dateRange: Array<Date>): string {
+    const hasDateRange = dateRange?.length
+    if (!hasDateRange) {
+        // Delete all
+        return t(msg => msg.item.operation.deleteConfirmMsgAll, { url })
+    }
+    const startDate = dateRange[0]
+    const endDate = dateRange[1]
+    const start = formatTime(startDate, DISPLAY_DATE_FORMAT)
+    const end = formatTime(endDate, DISPLAY_DATE_FORMAT)
+    return start === end
+        // Only one day
+        ? computeSingleConfirmText(url, start)
+        : t(msg => msg.item.operation.deleteConfirmMsgRange, { url, start, end })
+}
+
+const _default = defineComponent({
+    name: "OperationDeleteButton",
+    emits: ["confirm"],
+    props: {
+        mergeDate: Boolean,
+        // Filter of date range
+        dateRange: Array as PropType<Array<Date>>,
+        itemUrl: String,
+        itemDate: String
+    },
+    setup(props, ctx) {
+        const deleteMsg: Ref<string> = computed(() => {
+            const url = props.itemUrl
+            return props.mergeDate
+                ? computeRangeConfirmText(url, props.dateRange)
+                : computeSingleConfirmText(url, props.itemDate)
+        })
+        return () => h(OperationPopupConfirmButton, {
+            buttonIcon: Delete,
+            buttonType: "warning",
+            buttonText: deleteButtonText,
+            confirmText: deleteMsg.value,
+            onConfirm: () => ctx.emit("confirm"),
+        })
+    }
+})
+
+export default _default
