f3cfe0844786be747c44ce4642551c73b3adff1f
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-03-05 23:36:41 +0800

    Optimize file download

--- src/app/components/report/filter/download-file.ts
+++ src/app/components/report/filter/download-file.ts
@@ -7,86 +7,30 @@
 
 import { Download } from "@element-plus/icons"
 import { ElButton, ElDropdown, ElDropdownItem, ElDropdownMenu, ElIcon } from "element-plus"
-import { Ref, h } from "vue"
-import DataItem from "@entity/dto/data-item"
-import { exportCsv, exportJson } from "@util/file"
-import { t } from "@app/locale"
-import { dateFormatter, periodFormatter } from "../formatter"
+import { h, defineComponent } from "vue"
 
-type FileFormat = 'json' | 'csv'
+export type FileFormat = 'json' | 'csv'
 
-type ExportInfo = {
-    host: string
-    date?: string
-    total?: string
-    focus?: string
-    time?: number
-}
+const ALL_FILE_FORMATS: FileFormat[] = ["json", "csv"]
 
-type _Props = {
-    mergeDateRef: Ref<boolean>
-    dataRef: Ref<DataItem[]>
-    exportFileName: Ref<string>
-}
-
-export type DownloadFileProps = _Props
-
-/** 
- * @param rows row data
- * @returns data with json format 
- */
-const generateJsonData = (rows: DataItem[]) => {
-    return rows.map(row => {
-        const data: ExportInfo = { host: row.host }
-        // Always display by seconds
-        data.total = periodFormatter(row.total, true, true)
-        data.focus = periodFormatter(row.focus, true, true)
-        data.time = row.time
-        return data
-    })
-}
-
-/** 
- * @param rows row data
- * @returns data with csv format 
- */
-const generateCsvData = (rows: DataItem[], mergeDate: boolean) => {
-    let columnName: Array<keyof DataItem> = []
-    !mergeDate && columnName.push('date')
-    columnName = [...columnName, 'host', 'total', 'focus', 'time']
-    const data = [columnName.map(c => t(msg => msg.item[c]))]
-    rows.forEach(row => {
-        const csvR = []
-        !mergeDate && csvR.push(dateFormatter(row.date))
-        data.push([...csvR, row.host, periodFormatter(row.total, true), periodFormatter(row.focus, true), row.time])
-    })
-    return data
-}
-
-const exportFile = (props: _Props, format: FileFormat) => {
-    const rows = props.dataRef.value
-    const fileName = props.exportFileName.value
-    format === 'json' && exportJson(generateJsonData(rows), fileName)
-    format === 'csv' && exportCsv(generateCsvData(rows, props.mergeDateRef.value), fileName)
-}
-
-const dropButton = () => h<{}>(ElButton,
-    { size: 'mini', class: 'export-dropdown-button' },
-    () => h(ElIcon, { size: 17, style: { padding: '0 1px' } }, () => h(Download))
-)
-
-const dropdownMenuItem = (format: FileFormat, props: _Props) => h(ElDropdownItem, { onClick: () => exportFile(props, format) }, () => format)
-
-const dropdownMenu = (props: _Props) => h(ElDropdownMenu, {},
-    () => (['csv', 'json'] as FileFormat[]).map(format => dropdownMenuItem(format, props))
-)
-
-const downloadFile = (props: _Props) => h(ElDropdown,
-    { class: 'export-dropdown', showTimeout: 100 },
-    {
-        default: dropButton,
-        dropdown: () => dropdownMenu(props)
+const _default = defineComponent({
+    name: "FileDownload",
+    emits: ["download"],
+    setup(_, ctx) {
+        return () => h(ElDropdown, { class: 'export-dropdown', showTimeout: 100 }, {
+            default: () => h(ElButton,
+                { size: 'mini', class: 'export-dropdown-button' },
+                () => h(ElIcon, { size: 17, style: { padding: '0 1px' } }, () => h(Download))
+            ),
+            dropdown: () => h(ElDropdownMenu, {},
+                () => ALL_FILE_FORMATS.map(
+                    format => h(ElDropdownItem, {
+                        onClick: () => ctx.emit("download", format)
+                    }, () => format)
+                )
+            )
+        })
     }
-)
+})
 
-export default downloadFile
+export default _default
--- src/app/components/report/filter/index.ts
+++ src/app/components/report/filter/index.ts
@@ -8,19 +8,64 @@
 import { inputFilterItem, renderFilterContainer } from "@app/components/common/filter"
 import dateRange, { DateRangeFilterItemProps } from "./date-range-filter-item"
 import displayBySecond, { DisplayBySecondFilterItemProps } from "./display-by-second-filter-item"
-import downloadFile, { DownloadFileProps } from "./download-file"
+import DownloadFile, { FileFormat } from "./download-file"
 import mergeDate, { MergeDateFilterItemProps } from "./merge-date-filter-item"
 import mergeHost, { MergeHostFilterItemProps } from "./merge-host-filter-item"
-import { Ref } from "vue"
+import { Ref, h } from "vue"
+import DataItem from "@entity/dto/data-item"
+import { t } from "@app/locale"
+import { dateFormatter, periodFormatter } from "../formatter"
+import { exportCsv, exportJson } from "@util/file"
 
 export type FilterProps = DateRangeFilterItemProps
     & MergeDateFilterItemProps
     & MergeHostFilterItemProps
     & DisplayBySecondFilterItemProps
-    & DownloadFileProps
     & {
         hostRef: Ref<string>
+        dataRef: Ref<DataItem[]>
+        exportFileName: Ref<string>
     }
+
+/** 
+ * @param rows row data
+ * @returns data with csv format 
+ */
+const generateCsvData = (rows: DataItem[], mergeDate: boolean) => {
+    let columnName: Array<keyof DataItem> = []
+    !mergeDate && columnName.push('date')
+    columnName = [...columnName, 'host', 'total', 'focus', 'time']
+    const data = [columnName.map(c => t(msg => msg.item[c]))]
+    rows.forEach(row => {
+        const csvR = []
+        !mergeDate && csvR.push(dateFormatter(row.date))
+        data.push([...csvR, row.host, periodFormatter(row.total, true), periodFormatter(row.focus, true), row.time])
+    })
+    return data
+}
+
+type ExportInfo = {
+    host: string
+    date?: string
+    total?: string
+    focus?: string
+    time?: number
+}
+
+/** 
+ * @param rows row data
+ * @returns data with json format 
+ */
+const generateJsonData = (rows: DataItem[]) => {
+    return rows.map(row => {
+        const data: ExportInfo = { host: row.host }
+        // Always display by seconds
+        data.total = periodFormatter(row.total, true, true)
+        data.focus = periodFormatter(row.focus, true, true)
+        data.time = row.time
+        return data
+    })
+}
 
 const childNodes = (props: FilterProps) =>
     [
@@ -29,7 +74,14 @@ const childNodes = (props: FilterProps) =>
         ...mergeDate(props),
         ...mergeHost(props),
         ...displayBySecond(props),
-        downloadFile(props)
+        h(DownloadFile, {
+            onDownload(format: FileFormat) {
+                const rows = props.dataRef.value
+                const fileName = props.exportFileName.value
+                format === 'json' && exportJson(generateJsonData(rows), fileName)
+                format === 'csv' && exportCsv(generateCsvData(rows, props.mergeDateRef.value), fileName)
+            }
+        })
     ]
 
 export default renderFilterContainer(childNodes)
