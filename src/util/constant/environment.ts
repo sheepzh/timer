c79a1d97cf189bf8094a50c5a1607af8e815226c
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-03-27 21:07:07 +0800

    Hide upgrade link on the opera (#68)

--- src/util/constant/environment.ts
+++ src/util/constant/environment.ts
@@ -6,8 +6,30 @@
  */
 
 const { userAgent } = navigator
-export const IS_FIREFOX: boolean = /Firefox[\/\s](\d+\.\d+)/.test(userAgent)
+let isFirefox = false
+let isChrome = false
+let isEdge = false
+let isOpera = false
 
-export const IS_EDGE: boolean = userAgent.includes('Edg')
+if (/Firefox[\/\s](\d+\.\d+)/.test(userAgent)) {
+    isFirefox = true
+} else if (userAgent.includes('Edg')) {
+    // The Edge implements the chrome
+    isEdge = true
+} else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
+    // The Opera implements the chrome
+    isOpera = true
+} else if (userAgent.includes('Chrome')) {
+    isChrome = true
+}
 
-export const IS_CHROME: boolean = userAgent.includes('Chrome') && !userAgent.includes('Edg')
+export const IS_FIREFOX: boolean = isFirefox
+
+export const IS_EDGE: boolean = isEdge
+
+export const IS_CHROME: boolean = isChrome
+
+/**
+ * @since 0.7.2
+ */
+export const IS_OPERA: boolean = isOpera
