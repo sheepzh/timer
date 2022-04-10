039d5c830fb30bf20dffa602292875e3530f1949
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-04-10 14:59:47 +0800

    Refactor

---
+++ src/app/components/limit/modify/footer.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2021 Hengyang Zhang
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import { Check } from "@element-plus/icons-vue"
+import { ElButton } from "element-plus"
+import { defineComponent, h } from "vue"
+import { t } from "@app/locale"
+
+const buttonText = t(msg => msg.limit.button.save)
+const _default = defineComponent({
+    name: "SaveButton",
+    emits: ["save"],
+    setup(_, ctx) {
+        return () => h('span',
+            {},
+            h(ElButton, {
+                onClick: () => ctx.emit("save"),
+                type: 'primary',
+                icon: Check
+            }, () => buttonText)
+        )
+    }
+})
+
+export default _default
