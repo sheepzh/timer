83d1afc649dff72739f20f54ee0d46d4bbee7095
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-04-23 20:29:47 +0800

    Support the regular expression of port in merge rules (#75)

--- src/app/components/rule-merge/alert-info.ts
+++ src/app/components/rule-merge/alert-info.ts
@@ -7,14 +7,20 @@
 
 import { ElAlert } from "element-plus"
 import { t } from "@app/locale"
-import { h } from "vue"
+import { defineComponent, h } from "vue"
 import { MergeRuleMessage } from "@app/locale/components/merge-rule"
 
 const liKeys: (keyof MergeRuleMessage)[] = ['infoAlert0', 'infoAlert1', 'infoAlert2', 'infoAlert3', 'infoAlert4', 'infoAlert5']
+const title = t(msg => msg.mergeRule.infoAlertTitle)
 
-const alertInfo = () => h(ElAlert,
-    { type: 'info', title: t(msg => msg.mergeRule.infoAlertTitle) },
-    () => liKeys.map(key => h('li', t(msg => msg.mergeRule[key])))
-)
+const _default = defineComponent({
+    name: "RuleMergeAlertInfo",
+    render() {
+        return h(ElAlert,
+            { type: 'info', title },
+            () => liKeys.map(key => h('li', t(msg => msg.mergeRule[key])))
+        )
+    }
+})
 
-export default alertInfo
+export default _default
