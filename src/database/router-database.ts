dbaa6ed30f735e7e6eeb98e0e88f65cd59e890e4
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-05-10 23:10:54 +0800

    Add dashboard menu item and deprecate router history (#94)

--- src/database/router-database.ts
+++ src/database/router-database.ts
@@ -9,16 +9,29 @@ import BaseDatabase from "./common/base-database"
 import { REMAIN_WORD_PREFIX } from "./common/constant"
 
 const KEY = REMAIN_WORD_PREFIX + "app_router"
+
+/**
+ * @deprecated since 0.9.3
+ */
 class RouterDatabase extends BaseDatabase {
+    /**
+     * @deprecated since 0.9.3
+     */
     async getHistory(): Promise<string | undefined> {
         const items = await this.storage.get(KEY)
         return items[KEY]
     }
 
+    /**
+     * @deprecated since 0.9.3
+     */
     update(newRoute: string): Promise<void> {
         return this.setByKey(KEY, newRoute)
     }
 
+    /**
+     * @deprecated since 0.9.3
+     */
     async importData(_data: any): Promise<void> {
         // Do nothing
     }
