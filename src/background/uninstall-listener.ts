9a52c93abd5b82861513929749b9993b951ac5de
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-06-06 17:20:30 +0800

    Cancel gray for uninstall url

--- src/background/uninstall-listener.ts
+++ src/background/uninstall-listener.ts
@@ -5,27 +5,13 @@
  * https://opensource.org/licenses/MIT
  */
 
-import metaService from "@service/meta-service"
 import { UNINSTALL_QUESTIONNAIRE } from "@util/constant/url"
 import { locale } from "@util/i18n"
 
-/**
- * The percentage for gray
- */
-const GRAY_PERCENTAGE = 20
-
-function judgeGray(timeRand: Date) {
-    if (!timeRand) {
-        return false
-    }
-    return timeRand.getTime() % 100 < GRAY_PERCENTAGE
-}
-
 async function listen() {
     try {
-        const installTime = await metaService.getInstallTime()
         const uninstallUrl = UNINSTALL_QUESTIONNAIRE[locale]
-        uninstallUrl && judgeGray(installTime) && chrome.runtime.setUninstallURL(uninstallUrl)
+        uninstallUrl && chrome.runtime.setUninstallURL(uninstallUrl)
     } catch (e) {
         console.error(e)
     }
