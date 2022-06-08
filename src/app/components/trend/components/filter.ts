831a98af2f0f2ac317debae8c887fd6382705eb0
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-06-09 00:22:27 +0800

    Fix trend from report details (#105)

--- src/app/components/trend/components/filter.ts
+++ src/app/components/trend/components/filter.ts
@@ -4,9 +4,10 @@
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
+import type { Ref, PropType } from "vue"
 
 import { ElOption, ElSelect } from "element-plus"
-import { ref, Ref, h } from "vue"
+import { ref, h, defineComponent } from "vue"
 import timerService, { HostSet } from "@service/timer-service"
 import { daysAgo } from "@util/time"
 import { t } from "@app/locale"
@@ -15,47 +16,21 @@ import { TrendMessage } from "@app/locale/components/trend"
 import DateRangeFilterItem from "@app/components/common/date-range-filter-item"
 import { ElementDatePickerShortcut } from "@app/element-ui/date"
 
-type _Props = {
-    dateRangeRef: Ref<Date[]>
-    domainKeyRef: Ref<string>
-}
-
-export type FilterProps = _Props
-
-const trendSearchingRef: Ref<boolean> = ref(false)
-const trendDomainOptionsRef: Ref<HostOptionInfo[]> = ref([])
-
-// Host select
 const renderOption = (hostInfo: HostOptionInfo) => h(ElOption, { value: hostInfo.key(), label: hostInfo.toString() })
 
-const domainSelectOptions = () => trendDomainOptionsRef.value.map(domainInfo => renderOption(domainInfo))
-const handleRemoteSearch = async (queryStr: string) => {
+async function handleRemoteSearch(queryStr: string, trendDomainOptions: Ref<HostOptionInfo[]>, searching: Ref<boolean>) {
     if (!queryStr) {
-        trendDomainOptionsRef.value = []
+        trendDomainOptions.value = []
         return
     }
-    trendSearchingRef.value = true
+    searching.value = true
     const domains: HostSet = await timerService.listHosts(queryStr)
     const options: HostOptionInfo[] = []
     domains.origin.forEach(host => options.push(HostOptionInfo.origin(host)))
     domains.merged.forEach(host => options.push(HostOptionInfo.merged(host)))
-    trendDomainOptionsRef.value = options
-    trendSearchingRef.value = false
+    trendDomainOptions.value = options
+    searching.value = false
 }
-
-const domainSelect = ({ domainKeyRef }: _Props) => h(ElSelect,
-    {
-        placeholder: t(msg => msg.trend.hostPlaceholder),
-        class: 'filter-item',
-        modelValue: domainKeyRef.value,
-        clearable: true,
-        filterable: true,
-        remote: true,
-        loading: trendSearchingRef.value,
-        remoteMethod: (query: string) => handleRemoteSearch(query),
-        onChange: (key: string) => domainKeyRef.value = key,
-        onClear: () => domainKeyRef.value = ''
-    }, domainSelectOptions)
 
 function datePickerShortcut(msg: keyof TrendMessage, agoOfStart?: number, agoOfEnd?: number): ElementDatePickerShortcut {
     return {
@@ -63,26 +38,73 @@ function datePickerShortcut(msg: keyof TrendMessage, agoOfStart?: number, agoOfE
         value: daysAgo(agoOfStart || 0, agoOfEnd || 0)
     }
 }
-const shortcuts = [
+const SHORTCUTS = [
     datePickerShortcut('lateWeek', 7),
     datePickerShortcut('late15Days', 15),
     datePickerShortcut('late30Days', 30),
     datePickerShortcut("late90Days", 90)
 ]
+
+const HOST_PLACEHOLDER = t(msg => msg.trend.hostPlaceholder)
 // Date picker
-const startPlaceholder = t(msg => msg.trend.startDate)
-const endPlaceholder = t(msg => msg.trend.endDate)
-const datePickerItem = (props: _Props) => h(DateRangeFilterItem, {
-    defaultRange: props.dateRangeRef.value,
-    startPlaceholder,
-    endPlaceholder,
-    shortcuts,
-    onChange: (newVal: Date[]) => props.dateRangeRef.value = newVal,
-    disabledDate: (date: Date) => date.getTime() > new Date().getTime(),
+const START_DATE_PLACEHOLDER = t(msg => msg.trend.startDate)
+const END_DATE_PLACEHOLDER = t(msg => msg.trend.endDate)
+
+const _default = defineComponent({
+    name: "TrendFilter",
+    props: {
+        dateRange: Object as PropType<Date[]>,
+        defaultValue: Object as PropType<HostOptionInfo>
+    },
+    emits: ['change'],
+    setup(props, ctx) {
+        // @ts-ignore
+        const dateRange: Ref<Date[]> = ref(props.dateRange)
+        const domainKey: Ref<string> = ref('')
+        const trendSearching: Ref<boolean> = ref(false)
+        const trendDomainOptions: Ref<HostOptionInfo[]> = ref([])
+        const defaultOption: HostOptionInfo = props.defaultValue
+        if (defaultOption) {
+            domainKey.value = defaultOption.key()
+            trendDomainOptions.value.push(defaultOption)
+        }
+
+        function handleChange() {
+            const hostOption: HostOptionInfo = HostOptionInfo.from(domainKey.value)
+            ctx.emit('change', hostOption, dateRange.value)
+        }
+
+        return () => [h(ElSelect, {
+            placeholder: HOST_PLACEHOLDER,
+            class: 'filter-item',
+            modelValue: domainKey.value,
+            clearable: true,
+            filterable: true,
+            remote: true,
+            loading: trendSearching.value,
+            remoteMethod: (query: string) => handleRemoteSearch(query, trendDomainOptions, trendSearching),
+            onChange: (key: string) => {
+                domainKey.value = key
+                handleChange()
+            },
+            onClear: () => {
+                domainKey.value = ''
+                handleChange()
+            }
+        }, () => trendDomainOptions.value?.map(renderOption) || []),
+        h(DateRangeFilterItem, {
+            defaultRange: dateRange.value,
+            startPlaceholder: START_DATE_PLACEHOLDER,
+            endPlaceholder: END_DATE_PLACEHOLDER,
+            shortcuts: SHORTCUTS,
+            onChange: (newVal: Date[]) => {
+                dateRange.value = newVal
+                handleChange()
+            },
+            clearable: false,
+            disabledDate: (date: Date) => date.getTime() > new Date().getTime(),
+        })]
+    }
 })
 
-const filterItems = (props: _Props) => [domainSelect(props), datePickerItem(props)]
-
-export default (props: _Props) => filterItems(props)
-
-export function addToFilterOption(option: HostOptionInfo) { trendDomainOptionsRef.value.push(option) }
+export default _default
--- src/app/components/trend/index.ts
+++ src/app/components/trend/index.ts
@@ -6,61 +6,77 @@
  */
 
 import type { Ref } from "vue"
-import type { FilterProps } from "./components/filter"
 
-import { defineComponent, h, onMounted, ref, watch } from "vue"
+import { defineComponent, h, onMounted, ref } from "vue"
 import { useRoute, useRouter } from "vue-router"
 import { daysAgo } from "@util/time"
 import ContentContainer from "../common/content-container"
 import TrendChart from "./components/chart"
-import filterContainer, { addToFilterOption } from "./components/filter"
+import TrendFilter from "./components/filter"
 import HostOptionInfo from "./host-option-info"
+import timerService, { SortDirect, TimerQueryParam } from "@service/timer-service"
+import DataItem from "@entity/dto/data-item"
 
-type QueryParam = {
+type _Queries = {
     host: string
     merge: '1' | '0' | undefined
 }
 
-function initWithQuery(domainKey: Ref<string>, dateRange: Ref<Date[]>, chart: Ref) {
+function initWithQuery(hostOption: Ref<HostOptionInfo>) {
     // Process the query param
-    const query: QueryParam = useRoute().query as unknown as QueryParam
+    const query: _Queries = useRoute().query as unknown as _Queries
     useRouter().replace({ query: {} })
-    onMounted(() => {
-        const { host, merge } = query
-        // Init with queries
-        if (host) {
-            const option = new HostOptionInfo(host, merge === '1' || false)
-            addToFilterOption(option)
-            domainKey.value = option.key()
-        } else {
-            domainKey.value = ''
-        }
+    const { host, merge } = query
+    // Init with queries
+    host && (hostOption.value = new HostOptionInfo(host, merge === "1"))
+}
 
-        // Init here
-        chart.value.setDateRange(dateRange.value)
-    })
+async function query(hostOption: Ref<HostOptionInfo>, dateRange: Ref<Date[]>): Promise<DataItem[]> {
+    const hostVal = hostOption.value?.host
+    if (!hostVal) {
+        return []
+    }
+    const param: TimerQueryParam = {
+        // If the host is empty, no result will be queried with this param.
+        host: hostVal,
+        mergeHost: hostOption.value?.merged || false,
+        date: dateRange.value,
+        fullHost: true,
+        sort: 'date',
+        sortOrder: SortDirect.ASC
+    }
+    return await timerService.select(param)
 }
 
 const _default = defineComponent({
     name: "Trend",
     setup() {
-        const domainKey: Ref<string> = ref('')
         // @ts-ignore
         const dateRange: Ref<Date[]> = ref(daysAgo(7, 0))
+        const hostOption: Ref<HostOptionInfo> = ref()
         const chart: Ref = ref()
+        const filter: Ref = ref()
 
-        watch(domainKey, () => chart.value.setDomain(domainKey.value))
-        watch(dateRange, () => chart.value.setDateRange(dateRange.value))
+        initWithQuery(hostOption)
 
-        const filterProps: FilterProps = {
-            dateRangeRef: dateRange,
-            domainKeyRef: domainKey,
+        async function queryAndRender() {
+            const row = await query(hostOption, dateRange)
+            chart.value?.render(hostOption.value, dateRange.value, row)
         }
 
-        initWithQuery(domainKey, dateRange, chart)
+        onMounted(queryAndRender)
 
         return () => h(ContentContainer, {}, {
-            filter: () => filterContainer(filterProps),
+            filter: () => h(TrendFilter, {
+                defaultValue: hostOption.value,
+                dateRange: dateRange.value,
+                ref: filter,
+                onChange(newHostOption: HostOptionInfo, newDateRange: Date[]) {
+                    hostOption.value = newHostOption
+                    dateRange.value = newDateRange
+                    queryAndRender()
+                }
+            }),
             content: () => h(TrendChart, { ref: chart })
         })
     }
