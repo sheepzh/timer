4ec71bafa8b2ee3ba4ba6d89dc1489dd391d103a
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-07-02 14:01:22 +0800

    Sync timerData

--- src/background/chrome-synchronizer.ts
+++ src/background/chrome-synchronizer.ts
@@ -8,12 +8,15 @@ const sync = chrome.storage.sync
 
 async function syncData() {
     const current = new Date().getTime()
-    const meta = (await metaDb.getMeta())?.sync
-    syncTimer(current, 11)
+    const meta = await metaDb.getMeta()
+    if (meta.sync) {
+        meta.sync = {}
+    }
+    syncTimer(current, meta)
 }
 
-async function syncTimer(current: number, lastSyncTs: number) {
-    const startTime = lastSyncTs
+async function syncTimer(current: number, meta: timer.meta.ExtensionMeta) {
+    const startTime = meta.sync.timer
     const endTime = current - MILL_PER_DAY
     const localDb = new TimerDatabase(local)
     const syncDb = new TimerDatabase(sync)
@@ -25,11 +28,11 @@ async function syncTimer(current: number, lastSyncTs: number) {
 
     const localItems = await localDb.select({ date: dateRange })
     const remoteItems = await syncDb.select({ date: dateRange })
-    localItems.forEach(item => {
-        syncDb.accumulate(item.host, item.date, item)
-    })
+    await Promise.all(localItems.map(item => syncDb.accumulate(item.host, item.date, item)))
+    await Promise.all(remoteItems.map(item => localDb.accumulate(item.host, item.date, item)))
 
-    console.log(localItems, remoteItems)
+    meta.sync.timer = current
+    await metaDb.update(meta)
 }
 
 export default class ChromeSynchronizer {
