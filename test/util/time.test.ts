e7b4c6650f54347d629134b710c506966bc367cb
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-07-04 22:42:43 +0800

    Optimize test coverage

--- test/util/time.test.ts
+++ test/util/time.test.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2021 Hengyang Zhang
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 import { daysAgo, formatPeriod, formatPeriodCommon, formatTime, getMonthTime, getStartOfDay, getWeeksAgo, getWeekTime, isSameDay } from "@util/time"
 
 test('time', () => {
