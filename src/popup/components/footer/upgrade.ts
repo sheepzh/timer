b2e24d671e59ba5b4adba64a75aded6afdd1761f
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2022-07-24 00:44:33 +0800

    Fix upgrade link error (#128)

--- src/popup/components/footer/upgrade.ts
+++ src/popup/components/footer/upgrade.ts
@@ -12,11 +12,38 @@ import { UPDATE_PAGE } from "@util/constant/url"
 import { IS_FIREFOX } from "@util/constant/environment"
 import { IS_FROM_STORE } from "@util/constant/meta"
 
+/**
+ * Reset the position after upgrade showed
+ * 
+ * @param upgrade upgrade button
+ * @param upgradePopup label popup
+ */
+function observeUpgradeShowed(upgrade: HTMLElement, upgradePopup: HTMLElement) {
+    const observer = new MutationObserver(mutationList => mutationList.forEach(mutation => {
+        const causedByUpgradedDisplay = mutation.oldValue === "display: none"
+            && getComputedStyle(upgrade)?.getPropertyValue?.("display") === "block"
+        if (!causedByUpgradedDisplay) {
+            return
+        }
+        // Init the position of upgrade popup
+        const x = upgrade.getBoundingClientRect?.()?.left || 0 + window.scrollX || 0
+        x && (upgradePopup.style.transform = `translate(${x}px, -52px)`)
+    }))
+
+    observer.observe(upgrade, { attributes: true, attributeFilter: ["style"], attributeOldValue: true })
+
+    window.onunload = () => {
+        observer?.disconnect()
+        observer?.takeRecords()
+    }
+}
+
 function showUpgradeButton(latestVersion: string) {
     const upgrade = document.getElementById('upgrade-container')
     const upgradeLink = document.getElementById('upgrade-link')
     const upgradePopup = document.getElementById('upgrade-popup-container')
     const latestInfo = document.getElementById('latest-info')
+    observeUpgradeShowed(upgrade, upgradePopup)
 
     upgrade.style.display = 'block'
     upgrade.onmouseover = () => upgradePopup.style.display = 'block'
