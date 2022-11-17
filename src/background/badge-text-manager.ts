71f4d6a7ca2d55f59ea20b0529dbd147fc0fbc29
sheepzh <returnzhy1996@outlook.com>
2022-11-17 18:48:41 +0800

    Fix badge bugs if multiple windows open (#158)

--- src/background/badge-text-manager.ts
+++ src/background/badge-text-manager.ts
@@ -12,6 +12,17 @@ import { extractHostname, isBrowserUrl } from "@util/pattern"
 const storage = chrome.storage.local
 const timerDb: TimerDatabase = new TimerDatabase(storage)
 
+export type BadgeLocation = {
+    /**
+     * The tab id of badge text show display with
+     */
+    tabId: number
+    /**
+     * The host to gain the focus time, can be undefined or null
+     */
+    host: string
+}
+
 function mill2Str(milliseconds: number) {
     if (milliseconds < 60000) {
         // no more than 1 minutes
@@ -24,34 +35,50 @@ function mill2Str(milliseconds: number) {
     }
 }
 
-function setBadgeText(milliseconds: number | undefined) {
+function setBadgeText(milliseconds: number | undefined, tabId: number | undefined) {
     const text = milliseconds === undefined ? '' : mill2Str(milliseconds)
-    chrome.browserAction?.setBadgeText?.({ text })
+    chrome.browserAction?.setBadgeText?.({ text, tabId })
 }
 
-async function findActiveHost(): Promise<string> {
-    return new Promise(resolve => {
-        chrome.tabs.query({ active: true }, tabs => {
+function findFocusedWindow(): Promise<chrome.windows.Window> {
+    return new Promise(resolve =>
+        chrome.windows.getLastFocused(
+            // Only find normal window
+            { windowTypes: ['normal'] },
+            window => resolve(window && window.focused ? window : undefined)
+        )
+    )
+}
+
+function findActiveTab(): Promise<BadgeLocation> {
+    return new Promise(resolve => findFocusedWindow().then(window => {
+        if (!window) {
+            resolve(undefined)
+            return
+        }
+        chrome.tabs.query({ active: true, windowId: window.id }, tabs => {
             // Fix #131
             // Edge will return two active tabs, including the new tab with url 'edge://newtab/', GG
             tabs = tabs.filter(tab => !isBrowserUrl(tab.url))
             if (!tabs || !tabs.length) {
                 resolve(undefined)
             } else {
-                const { url } = tabs[0]
+                const { url, id } = tabs[0]
                 const host = extractHostname(url).host
-                resolve(host)
+                resolve({ tabId: id, host })
             }
         })
-    })
+    }))
 }
 
-async function updateFocus(host?: string) {
-    if (!host) {
-        host = await findActiveHost()
+async function updateFocus(badgeLocation?: BadgeLocation) {
+    badgeLocation = badgeLocation || await findActiveTab()
+    if (!badgeLocation) {
+        return
     }
+    const { host, tabId } = badgeLocation
     const milliseconds = host ? (await timerDb.get(host, new Date())).focus : undefined
-    setBadgeText(milliseconds)
+    setBadgeText(milliseconds, tabId)
 }
 
 const ALARM_NAME = 'timer-badge-text-manager-alarm'
@@ -62,7 +89,6 @@ function createAlarm(beforeAction?: () => void) {
 }
 
 class BadgeTextManager {
-    timer: NodeJS.Timer
     isPaused: boolean
 
     async init() {
@@ -72,7 +98,6 @@ class BadgeTextManager {
                 createAlarm(() => !this.isPaused && updateFocus())
             }
         })
-        // this.timer = setInterval(() => !this.isPaused && updateFocus(), 1000)
 
         const option: Partial<timer.option.AllOption> = await optionService.getAllOption()
         this.pauseOrResumeAccordingToOption(!!option.displayBadgeText)
@@ -84,7 +109,7 @@ class BadgeTextManager {
      */
     pause() {
         this.isPaused = true
-        setBadgeText(undefined)
+        setBadgeText(undefined, undefined)
     }
 
     /**
@@ -96,9 +121,9 @@ class BadgeTextManager {
         this.forceUpdate()
     }
 
-    forceUpdate(host?: string) {
+    forceUpdate(badgeLocation?: BadgeLocation) {
         if (this.isPaused) { return }
-        host ? updateFocus(host) : setBadgeText(undefined)
+        updateFocus(badgeLocation)
     }
 
     private pauseOrResumeAccordingToOption(displayBadgeText: boolean) {
