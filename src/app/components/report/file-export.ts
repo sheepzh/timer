00205f6c8e1c307e0c53d937d80ece57167670c4
ZHY <returnzhy1996@outlook.com>
2022-11-25 07:11:03 +0000

    Prettier code

---
+++ src/app/components/report/file-export.ts
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2022-present Hengyang Zhang
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import { t } from "@app/locale"
+import { formatTime } from "@util/time"
+import { periodFormatter } from "./formatter"
+import {
+    exportCsv as exportCsv_,
+    exportJson as exportJson_,
+} from "@util/file"
+
+type _ExportInfo = {
+    host: string
+    alias?: string
+    date?: string
+    total?: string
+    focus?: string
+    time?: number
+}
+
+/**
+ * Compute the name of downloaded file
+ */
+function computeFileName(filterParam: timer.app.report.FilterOption): string {
+    let baseName = t(msg => msg.report.exportFileName)
+    const { dateRange, mergeDate, mergeHost, timeFormat } = filterParam
+    if (dateRange && dateRange.length === 2) {
+        const start = dateRange[0]
+        const end = dateRange[1]
+        baseName += '_' + formatTime(start, '{y}{m}{d}')
+        baseName += '_' + formatTime(end, '{y}{m}{d}')
+    }
+    mergeDate && (baseName += '_' + t(msg => msg.report.mergeDate))
+    mergeHost && (baseName += '_' + t(msg => msg.report.mergeDomain))
+    timeFormat && (baseName += '_' + t(msg => msg.timeFormat[timeFormat]))
+    return baseName
+}
+
+const generateJsonData = (rows: timer.stat.Row[]) => rows.map(row => {
+    const data: _ExportInfo = { host: row.host }
+    data.date = row.date
+    data.alias = row.alias
+    // Always display by seconds
+    data.total = periodFormatter(row.total, "second", true)
+    data.focus = periodFormatter(row.focus, "second", true)
+    data.time = row.time
+    return data
+})
+
+/**
+ * Export json data
+ * 
+ * @param filterParam filter params
+ * @param rows row data
+ */
+export function exportJson(filterParam: timer.app.report.FilterOption, rows: timer.stat.Row[]): void {
+    const fileName = computeFileName(filterParam)
+    const jsonData = generateJsonData(rows)
+    exportJson_(jsonData, fileName)
+}
+
+function generateCsvData(rows: timer.stat.Row[], filterParam: timer.app.report.FilterOption): string[][] {
+    const { mergeDate, mergeHost } = filterParam
+    const columnName: string[] = []
+    if (!mergeDate) {
+        columnName.push(t(msg => msg.item.date))
+    }
+    columnName.push(t(msg => msg.item.host))
+    if (!mergeHost) {
+        columnName.push(t(msg => msg.siteManage.column.alias))
+    }
+    columnName.push(t(msg => msg.item.total))
+    columnName.push(t(msg => msg.item.focus))
+    columnName.push(t(msg => msg.item.time))
+    const data = [columnName]
+    rows.forEach(row => {
+        const line = []
+        if (!mergeDate) {
+            line.push(row.date)
+        }
+        line.push(row.host)
+        if (!mergeHost) {
+            line.push(row.alias || '')
+        }
+        line.push(periodFormatter(row.total, "second", true))
+        line.push(periodFormatter(row.focus, "second", true))
+        line.push(row.time)
+        data.push(line)
+    })
+    return data
+}
+
+/** 
+ * Export csv data
+ * 
+ * @param filterParam filter params
+ * @param rows row data
+ */
+export function exportCsv(filterParam: timer.app.report.FilterOption, rows: timer.stat.Row[]): void {
+    const fileName = computeFileName(filterParam)
+    const csvData = generateCsvData(rows, filterParam)
+    exportCsv_(csvData, fileName)
+}
