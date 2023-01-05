3cc4b97ade20bd68562b37f816dc02c037fa91c2
ZHY <returnzhy1996@outlook.com>
2023-01-05 03:09:34 +0000

    Guide page supports dark mode

--- src/guide/index.ts
+++ src/guide/index.ts
@@ -10,13 +10,15 @@ import 'element-plus/theme-chalk/index.css'
 
 import { initLocale } from "@i18n"
 import { t } from "./locale"
-import { init as initTheme } from "@util/dark-mode"
+import { init as initTheme, toggle } from "@util/dark-mode"
 import { createApp } from "vue"
 import Main from "./layout"
-
+import optionService from "@service/option-service"
 
 async function main() {
     initTheme()
+    // Calculate the latest mode
+    optionService.isDarkMode().then(toggle)
     await initLocale()
 
     const app = createApp(Main)
