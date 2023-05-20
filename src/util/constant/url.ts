679aab27b1cf11b6acb7cbe4305d8939d9979e6b
sheepzh <return1996@outlook.com>
2023-05-20 15:37:31 +0800

    Fix the homepage of Firefox (#213)

--- src/util/constant/url.ts
+++ src/util/constant/url.ts
@@ -8,7 +8,7 @@
 import { getRuntimeId } from "@api/chrome/runtime"
 import { IS_FIREFOX, IS_CHROME, IS_EDGE } from "./environment"
 
-export const FIREFOX_HOMEPAGE = 'https://addons.mozilla.org/firefox/addon/web%E6%99%82%E9%96%93%E7%B5%B1%E8%A8%88/'
+export const FIREFOX_HOMEPAGE = 'https://addons.mozilla.org/firefox/addon/besttimetracker'
 export const CHROME_HOMEPAGE = 'https://chrome.google.com/webstore/detail/%E7%BD%91%E8%B4%B9%E5%BE%88%E8%B4%B5-%E4%B8%8A%E7%BD%91%E6%97%B6%E9%97%B4%E7%BB%9F%E8%AE%A1/dkdhhcbjijekmneelocdllcldcpmekmm'
 export const EDGE_HOMEPAGE = 'https://microsoftedge.microsoft.com/addons/detail/timer-the-web-time-is-e/fepjgblalcnepokjblgbgmapmlkgfahc'
 
