daf77413d49e87c0dae010c5c3abbd5149729340
sheepzh <return1996@outlook.com>
2023-10-17 22:11:13 +0800

    Fix crash on Qihu-360X Browser

--- src/api/chrome/i18n.ts
+++ src/api/chrome/i18n.ts
@@ -1,10 +1,10 @@
+// Bug of chrome:
+// chrome.i18n.getUILanguage may not work in background
 export function getUILanguage(): string {
-    return chrome.i18n.getUILanguage()
+    return chrome?.i18n?.getUILanguage?.()
 }
 
-// Bug of chrome： 
+// Bug of chrome：
 // chrome.i18n.getMessage may not work in background
 // @see https://stackoverflow.com/questions/6089707/calling-chrome-i18n-getmessage-from-a-content-script
-export function getMessage(messageName: string): string {
-    return chrome.i18n.getMessage(messageName)
-}
+export const getMessage: (key: string) => string = chrome?.i18n?.getMessage
--- src/i18n/chrome/t.ts
+++ src/i18n/chrome/t.ts
@@ -6,8 +6,16 @@
  */
 
 import { getMessage } from "@api/chrome/i18n"
-import { router, ChromeMessage } from "./message"
+import messages, { router, ChromeMessage } from "./message"
+import { IS_CHROME } from "@util/constant/environment"
+import { t } from ".."
 
 export const keyPathOf = (key: (root: ChromeMessage) => string) => key(router)
 
-export const t2Chrome = (key: (root: ChromeMessage) => string) => getMessage(keyPathOf(key))
+export const t2Chrome = (key: (root: ChromeMessage) => string) => {
+    if (getMessage) {
+        return getMessage(keyPathOf(key))
+    }
+    console.error(IS_CHROME)
+    return t<ChromeMessage>(messages, { key }, 'en')
+}
